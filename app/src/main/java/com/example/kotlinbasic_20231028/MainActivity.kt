7058e7e57966f34f0f7ae265918c5dd70b1aa8f3
rhythmshare <randomcony@gmail.com>
2023-10-28 16:39:11 +0900

    kotlin condition - if is the same as java - when syntax

--- app/src/main/java/com/example/kotlinbasic_20231028/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic_20231028/MainActivity.kt
@@ -44,12 +44,49 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, myName, Toast.LENGTH_SHORT).show()
             
             // 너구리 이름 : 변경 불가
-            val raccoonName : String
-            raccoonName = "kun"
+            val raccoonName : String = "kun"
             
             // 너구리 이름 변경 시도
-            //raccoonName = "coon"   => syntax error 상수로 선언한 공간의 값을 변경 시도함   
+            //raccoonName = "coon"   => syntax error 상수로 선언한 공간의 값을 변경 시도함
 
+            // 내 출생년도 var 에 저장. (변경 가능) => 변수 생성과 동시에 저장
+            var myBirthYear = 1989  // 자료형이 자동으로 Int로 추론되어 결정됨 (자료형이 고정되어 다른 종류의 값 대입 불가)
+
+            // 코틀린에서는 모든 변수가 전부 참조형 변수. 코틀린은 모든 변수가 다 .을찍어서 부가기능 활용 가능(메서드 등)
+            // myBirthYear.compareTo() => Int 도 참조형. 추가 기능 활용 가능
+
+
+        }
+
+        binding.btnCondition.setOnClickListener {
+            // 사용자의 나이를 val에 저장
+            val userAge = 25
+
+            // 20살 이상? "성인" 문구 토스트로
+//            if (userAge >= 20){
+//                Toast.makeText(this, "성인입니다.",Toast.LENGTH_SHORT).show()
+//            }
+//            else if(userAge >=17){
+//                Toast.makeText(this, "중학생입니다.",Toast.LENGTH_SHORT).show()
+//            }
+//            else {
+//                Toast.makeText(this, "그 이하입니다.",Toast.LENGTH_SHORT).show()
+//            }
+        // when 활용 예시
+            when(userAge){
+                25 -> {
+                    // userAge에 25가 들어있을 때 실행할 코드
+                    Toast.makeText(this, "25살 입니다.", Toast.LENGTH_SHORT).show()
+                }
+                27, 28, 29 -> {
+                    // userAge의 값이 27, 28, 29 중 하나라면 실행
+                    Toast.makeText(this, "20대 후반입니다.",Toast.LENGTH_SHORT).show()
+                }
+                in 30..39->{
+                    // userAge의 값이 30 이상 39 이하인 경우
+                    Toast.makeText(this, "30대 입니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
